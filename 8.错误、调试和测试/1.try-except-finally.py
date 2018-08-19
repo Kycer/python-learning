9032999c983392fa88ac91bb2f3950a245f443b5
yksoul <978262587@qq.com>
2018-08-19 21:55:05 +0800

    错误

---
+++ 8.错误、调试和测试/1.try-except-finally.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+try:
+    print('try...')
+    r = 10 / int('2')
+    print('result:', r)
+except ValueError as e:
+    print('ValueError:', e)
+except ZeroDivisionError as e:
+    print('ZeroDivisionError:', e)
+else:
+    print('no error!')
+finally:
+    print('finally...')
+print('END')
+
+import logging
+
+
+# Python内置的logging模块可以非常容易地记录错误信息：
+# 同样是出错，但程序打印完错误信息后会继续执行，并正常退出：
+def foo(s):
+    return 10 / int(s)
+
+
+def bar(s):
+    return foo(s) * 2
+
+
+def main():
+    try:
+        bar('0')
+    except Exception as e:
+        logging.exception(e)
+
+
+main()
+print('END')
+
+
+# 抛出错误  raise
+class FooError(ValueError):
+    pass
+
+
+def foo(s):
+    n = int(s)
+    if n == 0:
+        raise FooError('invalid value: %s' % s)
+    return 10 / n
+
+
+foo('0')
